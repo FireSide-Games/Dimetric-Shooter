b91ac25b1d89f4f95757cd4e3bf51d1dfc6a6fbf
Avahe Kellenberger <avahe@protonmail.ch>
2020-01-17 20:33:57 -0500

    Adjusted sprite locations.

--- pistol.gd
+++ pistol.gd
@@ -6,7 +6,6 @@ extends Gun
 func fire(seconds_since_fired: float) -> bool:
 	var can_fire: bool = .fire(seconds_since_fired)
 	if can_fire:
-		# TODO: This should be handled by an AnimationTree in the future.
 		$AnimationPlayer.play("fire")
 		$AnimationPlayer.queue("idle")
 	return can_fire
