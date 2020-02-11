4670bf368484992dc1873a201062b54349482769
Avahe Kellenberger <avahe@protonmail.ch>
2020-02-10 21:29:21 -0500

    HUD is now added via code.
    This prevents the HUD taking up a bunch of visual space in the editor.

--- world1.gd
+++ world1.gd
@@ -2,4 +2,7 @@ extends Node2D
 
 func _ready():
 	Global.MainScene = $YSort
-	Global.HUD = $HUD
+	Global.HUD = preload("res://hud/HUD.tscn").instance()
+	Global.HUD.scale = Vector2(4, 4)
+	Global.HUD.offset = Vector2(10, 10)
+	Global.MainScene.add_child(Global.HUD)
